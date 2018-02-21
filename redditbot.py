26dcff5be93bcaf05ea5a02e6b4953399d1e3690
DESKTOP-IU9UJO6\Peter <morrisonman56@gmail.com>
2018-02-20 19:09:43 -0700

    move praw related functions to reddit_praw.py
    - there will be more praw related code, so it's best to move it now

--- redditbot.py
+++ redditbot.py
@@ -1,21 +1,6 @@
-import praw
 import time
 import imagehandler
-import request_history
-
-
-def authenticate():
-    # Requires the praw.ini file, the format can be found at https://praw.readthedocs.io/en/latest/index.html
-    reddit = praw.Reddit('Binger_Bot', user_agent='script: reddit_binge_bot:v0.1 (by /u/Binger_Bot)')
-    print('Authenticated as {}\n'.format(reddit.user.me()))
-    return reddit
-
-
-def fetch_requests(reddit):
-    # Finds subreddit strings in the top-most level comments (ex: '/r/subredditname')
-    submission = reddit.submission(url='https://www.reddit.com/r/RedditBinge/comments/6us5kk/'
-                                       'comment_in_this_thread_to_request_a_subreddit_to/')
-    request_history.save_requests(submission)
+import reddit_praw
 
 
 def main():
@@ -24,9 +9,10 @@ def main():
     title = "test"  # test var
     # test vars above will be removed when reading directly from subreddit is implemented
 
-    reddit = authenticate()
+    reddit = reddit_praw.authenticate()
     while True:
-        fetch_requests(reddit)
+        reddit_praw.fetch_requests(reddit)
+
         # skips imgur links that have a removed image
         if "removed" in url:
             print("Image was removed from imgur from post:" + title + "\n")
@@ -38,4 +24,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
